17812fa6c7585f164166deb0ce41874fc1d8ef4f
Adrien Russo <adrien@appro-auto.com>
2025-08-02 19:14:49 +0200

    Remove unused Stimulus controllers and clean up CSRF protection logic

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,2 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+// Fichier vidé car Stimulus n'est plus utilisé
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
